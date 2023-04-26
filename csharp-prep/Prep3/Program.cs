52bb175bfe18f2e32f5e59bb93d331a0a1159b71
roasmarialaura <roa20003@byui.edu>
2023-04-26 09:23:56 -0300

    Completed Guess My number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int number = randomGenerator .Next(1, 101);
+        int guess = -1;
+
+        while (number != guess)
+        {
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+       
+        }
+    Console.WriteLine("You guessed it!");
+
     }
 }
